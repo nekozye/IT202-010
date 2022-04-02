25d4d51ff6d57e21f0a2176539a814c16e14972d
nekozye <nekozeye@gmail.com>
2022-04-02 17:25:28 -0400

    added helper function

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -3,10 +3,11 @@
 ?>
 <h1>Home</h1>
 <?php
-    if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
-    echo "Welcome, " . $_SESSION["user"]["email"]; 
+//edited to use user_helpers.php
+    if(is_logged_in()){
+        echo "Welcome, " . get_user_email(); 
     }
     else{
-    echo "You're not logged in";
+        echo "You're not logged in";
     }
 ?>
