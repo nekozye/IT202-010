d0198d5c054715d7bb4e8b5f7289dbd35c1994d8
nekozye <nekozeye@gmail.com>
2022-04-02 17:03:50 -0400

    adding helper phps

--- lib/functions.php
+++ lib/functions.php
@@ -1,11 +1,17 @@
 <?php
 //TODO 1: require db.php
 require_once(__DIR__ . "/db.php");
+
+
+//helper for redirecting project path.
+$BASE_PATH = "/Project/";
+
 //require safer_echo.php
 require(__DIR__ . "/safer_echo.php");
 //TODO 2: filter helpers
-
+require(__DIR__ . "/sanitizers.php");
 //TODO 3: User helpers
-
+require(__DIR__ . "/user_helpers.php");
 //TODO 4: Flash Message Helpers
+require(__DIR__ . "/flash_messages.php");
 ?>
