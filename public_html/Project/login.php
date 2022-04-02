44e5e0f8b037890de31ebb360b19d1d9bd41840b
nekozye <nekozeye@gmail.com>
2022-04-02 17:57:12 -0400

    adding profile default for merging section

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -66,7 +66,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                   $hash = $user["password"];
                   unset($user["password"]);
                   if (password_verify($password, $hash)) {
-                      flash("Weclome $email");
+                      flash("Welcome $email");
                   } else {
                       flash("Invalid password");
                   }
