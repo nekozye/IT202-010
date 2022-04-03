ee8ef655dd31a50646118c489bb44154784de4ae
nekozye <nekozeye@gmail.com>
2022-04-03 19:36:15 -0400

    javascript validation

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -44,11 +44,6 @@ require(__DIR__ . "/../../partials/nav.php");
             flash("Password is too short", "danger");
         }
 
-        if(!isValid)
-        {
-            password.value = "";
-        }
-
         document.blur();
 
         return isValid;
